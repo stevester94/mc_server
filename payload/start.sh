1f56a927a8883c11cfe1140dbd5c8d3b5b02551b
Steven Mackey <stevester94@gmail.com>
2020-07-18 01:48:24 -0700

    Fix retarded vanilla server mistake, downgrade javva

--- payload/start.sh
+++ payload/start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -server -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -Xmx6124M -Xms4096M -jar  minecraft_server.1.7.10.jar
+#java -server -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -Xmx6124M -Xms4096M -jar  minecraft_server.1.7.10.jar
+java -server -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -Xmx6124M -Xms4096M -jar  forge-1.7.10-10.13.4.1614-1.7.10-universal.jar nogui
